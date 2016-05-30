797f7878488d09c1bf21fe48070d2c6fbce4b635
miamano <frinyo@hotmail.com>
2016-05-30 13:55:03 +0200

    First lab added

---
+++ labb1/images/resize.bat
@@ -0,0 +1,6 @@
+convert 1.jpg -auto-orient 11.jpg
+convert 2.jpg -auto-orient 22.jpg
+convert 3.jpg -auto-orient 33.jpg
+convert 4.jpg -auto-orient 44.jpg
+convert 5.jpg -auto-orient 55.jpg
+convert 6.jpg -auto-orient 66.jpg
