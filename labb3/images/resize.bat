f8355d3721f93b32b4db9ab4748c797b242c4852
miamano <frinyo@hotmail.com>
2016-06-01 14:48:34 +0200

    labb3 adds

---
+++ labb3/images/resize.bat
@@ -0,0 +1,6 @@
+convert 1.jpg -auto-orient -resize 244x244 11.jpg
+convert 2.jpg -auto-orient -resize 244x244 22.jpg
+convert 3.jpg -auto-orient -resize 244x244 33.jpg
+convert 4.jpg -auto-orient -resize 244x244 44.jpg
+convert 5.jpg -auto-orient -resize 244x244 55.jpg
+convert 6.jpg -auto-orient -resize 244x244 66.jpg
