b0b9dba3fcf986a0d80f3c64584598a58ea90492
miamano <frinyo@hotmail.com>
2016-06-07 00:57:11 +0200

    labb4 första utkast: basen, fotos, about klar

---
+++ labb4/island/island/images/resize.bat
@@ -0,0 +1,7 @@
+convert 1.jpg -auto-orient -resize 740x400 photo6.jpg
+convert 1.jpg -auto-orient -resize 740x400 photo7.jpg
+convert 1.jpg -auto-orient -resize 740x400 photo8.jpg
+convert 1.jpg -auto-orient -resize 740x400 photo9.jpg
+convert 1.jpg -auto-orient -resize 740x400 photo10.jpg
+convert 1.jpg -auto-orient -resize 740x400 photo11.jpg
+convert 1.jpg -auto-orient -resize 740x400 images.jpg
