89291d833441a7176a6beb93b4d7196de09de484
miamano <frinyo@hotmail.com>
2016-06-08 10:01:49 +0200

    Alla grundfunktioner. TODO: fixa index.html background

--- labb4/island/island/scripts/contact.js
+++ labb4/island/island/scripts/contact.js
@@ -1,16 +1,36 @@
 ﻿$(document).ready(function () {
    
-
-
-
     //jQuery('action1').on('click', function (e) { document.getElementById('happy').style.visibility = 'visible'; e.preventDefault();});
     //jQuery('action2').on('click', function (e) { document.getElementById('neutral').style.visibility = 'visible'; e.preventDefault(); });
     //jQuery('action3').on('click', function (e) { document.getElementById('sad').style.visibility = 'visible'; e.preventDefault(); });
 
     //$('#choicelist').on('click', result);
+
+    $('#action1').click(function (e) {
+        document.getElementById('happy').style.visibility = 'visible';
+        document.getElementById('neutral').style.visibility = 'hidden';
+        document.getElementById('sad').style.visibility = 'hidden';
+        e.preventDefault();
+    });
+    $('#action2').click(function (e) {
+        document.getElementById('neutral').style.visibility = 'visible';
+        document.getElementById('happy').style.visibility = 'hidden';
+        document.getElementById('sad').style.visibility = 'hidden';
+        e.preventDefault();
+    });
+    $('#action3').click(function (e) {
+        document.getElementById('sad').style.visibility = 'visible';
+        document.getElementById('happy').style.visibility = 'hidden';
+        document.getElementById('neutral').style.visibility = 'hidden';
+        e.preventDefault();
+    });
 });
 
-function result() {
+//function result() {
     //$('#datebox').val($(this).text());
-    //document.getElementById('happy').style.visibility = 'visible';
-}
+    //$('#choicelist').val($(this).text())
+    //var a = document.getElementById("#choicelist");
+
+    //document.getElementById(a.options[a.selectedIndex].value).style.visibility = 'visible';
+   
+//}
